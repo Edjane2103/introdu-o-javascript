b5ea65fea903edee2ceb17b9b4c89b8d036680a2
Edjane2103 <edjaneferreira109@gmail.com>
2024-09-25 11:34:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function solicitardados() {
+//solicitar o nome do usuário
+let nome = prompt("qual o seu nome");
+
+//solicitar a idade do usuário
+let idade = prompt("qual a sua idade");
+
+//exibe a saudação
+alert("ola, "+nome +"!vc tem" + idade + "anos.");
+
+//mostra os dados no console
+console.log("nome do usuário:;", nome);
+console.log("idade do usuário:", idade);
+}
+    
+
+
+
+
